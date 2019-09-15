6ddb69fa27ab33a01f8479f5c3a803ad4729f34e
zafarbutt <zafarbutt03@outlook.com>
2019-09-14 23:34:43 -0700

    intial comit

---
+++ CurdOperation/CurdOperation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurdOperation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
